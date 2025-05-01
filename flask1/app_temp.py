cc162aee5ce507d4ae5fb939bd4a1ffba150104d
ChoJunyong <junyong4510@gmail.com>
2025-05-01 18:39:14 +0900

    init

---
+++ flask1/app_temp.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() :
+    return render_template('app_index.html')
+
+@app.route('/input')
+def input():
+    return render_template('app_input.html')
+
+@app.route('/result',method=['POST'])
+def result() :
+    names = request.form.getlist('name[]')
+    student_number = request.form.getlist('StudentNumber[]')
+    return render_template('app_result.html',students=zip(names,student_number))
+
+@app.route('/contact')
+def contact_info() :
+    return render_template('app_contact.html')
+
+if __name__=='__main__' :
+    app.run(debug=True)
