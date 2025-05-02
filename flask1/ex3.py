9acb050ad3f5f8f38067e0670cc8e52984d2f3a3
ChoJunyong <junyong4510@gmail.com>
2025-05-02 13:06:22 +0900

    input.html

--- flask1/ex3.py
+++ flask1/ex3.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 def input() :
     return render_template('input.html')
 
-@app.route('/result',method=['POST','GET'])
+@app.route('/result',methods=['POST','GET'])
 def result() :
     if(request.method=='POST'):
         result = dict()
         result['Name'] = request.form.get('name')
         result['StuedntNumber'] = request.form.get('StudentNumber')
-        result['language'] = request.form.getlist('languages')
-        result['languages'] = ','.join(result['languages'])
-        return render_template('result.html',result=result)
+        # result['language'] = request.form.getlist('languages')
+        # result['languages'] = ','.join(result['languages'])
+        # return render_template('result.html',result=result)
     
 app.run(debug=1)
