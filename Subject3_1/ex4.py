2cea3f024a674c4c79d409c4827949549eb72960
ChoJunyong <junyong4510@gmail.com>
2025-05-04 21:03:32 +0900

    Subject3_1

---
+++ Subject3_1/ex4.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def input() :
+    return render_template('input.html')
+
+@app.route('/result',methods=['POST','GET'])
+def result() :
+    if(request.method=='POST'):
+        result = dict()
+        result['Name'] = request.form.get('name')
+        result['StuedentNumber'] = request.form.get('StudentNumber')
+        result['Email'] = request.form.get('Full-email')
+        result['Dream girl/guy'] = request.form.get('Dream')
+        # result['language'] = request.form.getlist('languages')
+        # result['languages'] = ','.join(result['languages'])
+        # return render_template('result.html',result=result)
+    return render_template('result.html', result=result)
+    
+app.run(debug=1)
