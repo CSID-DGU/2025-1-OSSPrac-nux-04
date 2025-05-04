30c6a1d25183c74fce80ddd15bdd15ba491a9106
won172 <swc3466@naver.com>
2025-05-04 22:44:03 +0900

    languages 추가

--- Subject3_1/ex4.py
+++ Subject3_1/ex4.py
@@ -12,8 +12,8 @@ def result() :
         result = dict()
         result['Name'] = request.form.get('name')
         result['StuedentNumber'] = request.form.get('StudentNumber')
-        # result['language'] = request.form.getlist('languages')
-        # result['languages'] = ','.join(result['languages'])
+        result['languages'] = request.form.getlist('languages')
+        result['languages'] = ','.join(result['languages'])
         # return render_template('result.html',result=result)
     return render_template('result.html', result=result)
     
