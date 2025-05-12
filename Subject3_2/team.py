0e216164584140c10dd922595c265fe382eeda68
won172 <swc3466@naver.com>
2025-05-12 21:42:22 +0900

    역할, 성별, 좋아하는 음식 추가

--- Subject3_2/team.py
+++ Subject3_2/team.py
@@ -13,10 +13,13 @@ def input() :
 @app.route('/result',methods=['POST'])
 def result():
     names = request.form.getlist('name[]')
+    role = request.form.getlist('Role[]')
     student_numbers = request.form.getlist('StudentNumber[]')
     emails = request.form.getlist("email[]")
+    genders = request.form.getlist('gender[]')
+    foods   = request.form.getlist('favorite_food[]')
     
-    return render_template('result.html',students=zip(names,student_numbers,emails))
+    return render_template('result.html',students=zip(names,role,student_numbers,emails,genders,foods))
 
 @app.route('/contact')
 def contact_info():
