29541b1711915fe3fce8292d8e82d07b72b5ce83
jaeyun <nick0824@dgu.ac.kr>
2025-05-13 01:25:59 +0900

    학과, 사진 추가

--- Subject3_2/team.py
+++ Subject3_2/team.py
@@ -14,11 +14,12 @@ def input() :
 def result():
     names = request.form.getlist('name[]')
     student_numbers = request.form.getlist('StudentNumber[]')
+    department = request.form.getlist('Department[]')
     emails = request.form.getlist("email[]")
     dreams = request.form.getlist('Dream[]')
     phonenumber = request.form.getlist('Phonenumber[]')
     MBTI = request.form.getlist('MBTI[]')
-    return render_template('result.html',students=zip(names,student_numbers,emails,dreams,phonenumber, MBTI))    
+    return render_template('result.html',students=zip(names,student_numbers,department,emails,dreams,phonenumber, MBTI))    
 
 
 @app.route('/contact')
